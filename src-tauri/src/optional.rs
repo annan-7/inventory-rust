3b1ac6139f04c2bce253fa7e1969c65d8eb9dc86
annan-7 <annanrecord@gmail.com>
2025-07-07 18:52:09 -0400

    updates

---
+++ src-tauri/src/optional.rs
@@ -0,0 +1,16 @@
+// src-tauri/src/optional.rs
+use rusqlite::Result;
+
+pub trait OptionalExtension<T> {
+    fn optional(self) -> Result<Option<T>>;
+}
+
+impl<T> OptionalExtension<T> for Result<T> {
+    fn optional(self) -> Result<Option<T>> {
+        match self {
+            Ok(value) => Ok(Some(value)),
+            Err(rusqlite::Error::QueryReturnedNoRows) => Ok(None),
+            Err(e) => Err(e),
+        }
+    }
+}
